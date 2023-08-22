6ceec125e5e77fb0825bb24151b1544eb7130118
oscarmojica <martinezmojicaoscar@gmail.com>
2023-08-22 07:07:51 -0500

    Se agrego el Proyecto

---
+++ Controllers/SwaggerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba_Dev_front.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwaggerController : ControllerBase
+    {
+        List<Swagger> swaggers = new List<Swagger>()
+        {
+            new Swagger() { Id=1, Name = "Oscar"},
+            new Swagger() { Id=2, Name = "Martinez"},
+            new Swagger() { Id=3, Name = "Mojica"},    
+        };
+        [HttpGet]
+        public ActionResult<Swagger> Get(int Id) 
+        {
+             var swagger = swaggers.Where(d=>d.Id == Id).FirstOrDefault();
+            if(swagger == null) return NotFound();
+            return swagger;
+        }
+    }
+
+    public class Swagger
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
